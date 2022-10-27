d12c93396abb9d60c241be781bcf2a7d2a94063a
Joe Crawford <joseph.crawford@bdi.ox.ac.uk>
2022-10-27 16:50:47 +0100

    WIP: Add section importing, use column names as metadata keys

--- grails-app/services/uk/ac/ox/softeng/maurodatamapper/plugins/openclinica/v3/importer/OpenClinicaV3CrfDataModelImporterProviderService.groovy
+++ grails-app/services/uk/ac/ox/softeng/maurodatamapper/plugins/openclinica/v3/importer/OpenClinicaV3CrfDataModelImporterProviderService.groovy
@@ -22,6 +22,8 @@ import uk.ac.ox.softeng.maurodatamapper.api.exception.ApiInternalException
 import uk.ac.ox.softeng.maurodatamapper.api.exception.ApiNotYetImplementedException
 import uk.ac.ox.softeng.maurodatamapper.api.exception.ApiUnauthorizedException
 import uk.ac.ox.softeng.maurodatamapper.core.facet.Metadata
+import uk.ac.ox.softeng.maurodatamapper.core.model.CatalogueItem
+import uk.ac.ox.softeng.maurodatamapper.core.model.facet.MetadataAware
 import uk.ac.ox.softeng.maurodatamapper.core.provider.importer.parameter.FileParameter
 import uk.ac.ox.softeng.maurodatamapper.datamodel.DataModel
 import uk.ac.ox.softeng.maurodatamapper.datamodel.DataModelService
@@ -31,6 +33,11 @@ import uk.ac.ox.softeng.maurodatamapper.datamodel.provider.importer.DataModelImp
 import uk.ac.ox.softeng.maurodatamapper.datamodel.provider.importer.parameter.DataModelFileImporterProviderServiceParameters
 import uk.ac.ox.softeng.maurodatamapper.plugins.excel.datamodel.provider.importer.SimpleExcelDataModelImporterProviderService
 import uk.ac.ox.softeng.maurodatamapper.plugins.openclinica.v3.crf.OpenClinicaV3CrfProfileProviderService
+import uk.ac.ox.softeng.maurodatamapper.plugins.openclinica.v3.crf.section.OpenClinicaV3CrfSectionProfileProviderService
+import uk.ac.ox.softeng.maurodatamapper.profile.domain.ProfileField
+import uk.ac.ox.softeng.maurodatamapper.profile.object.JsonProfile
+import uk.ac.ox.softeng.maurodatamapper.profile.object.Profile
+import uk.ac.ox.softeng.maurodatamapper.profile.provider.ProfileProviderService
 import uk.ac.ox.softeng.maurodatamapper.security.User
 
 import org.apache.poi.ss.usermodel.Sheet
@@ -49,10 +56,38 @@ class OpenClinicaV3CrfDataModelImporterProviderService extends DataModelImporter
                                                                instructions : 'INSTRUCTIONS',
                                                                pageNumber   : 'PAGE_NUMBER',
                                                                parentSection: 'PARENT_SECTION']
+    static final Map<String, String> ITEMS_SHEET_COLUMNS = [itemName                    : 'ITEM_NAME',
+                                                            descriptionLabel            : 'DESCRIPTION_LABEL',
+                                                            leftItemText                : 'LEFT_ITEM_TEXT',
+                                                            units                       : 'UNITS',
+                                                            rightItemText               : 'RIGHT_ITEM_TEXT',
+                                                            sectionLabel                : 'SECTION_LABEL',
+                                                            groupLabel                  : 'GROUP_LABEL',
+                                                            header                      : 'HEADER',
+                                                            subheader                   : 'SUBHEADER',
+                                                            parentItem                  : 'PARENT_ITEM',
+                                                            columnNumber                : 'COLUMN_NUMBER',
+                                                            pageNumber                  : 'PAGE_NUMBER',
+                                                            questionNumber              : 'QUESTION_NUMBER',
+                                                            responseType                : 'RESPONSE_TYPE',
+                                                            responseLabel               : 'RESPONSE_LABEL',
+                                                            responseOptionsText         : 'RESPONSE_OPTIONS_TEXT',
+                                                            responseValuesOrCalculations: 'RESPONSE_VALUES_OR_CALCULATIONS',
+                                                            responseLayout              : 'RESPONSE_LAYOUT',
+                                                            defaultValue                : 'DEFAULT_VALUE',
+                                                            dataType                    : 'DATA_TYPE',
+                                                            widthDecimal                : 'WIDTH_DECIMAL',
+                                                            validation                  : 'VALIDATION',
+                                                            validationErrorMessage      : 'VALIDATION_ERROR_MESSAGE',
+                                                            phi                         : 'PHI',
+                                                            required                    : 'REQUIRED',
+                                                            itemDisplayStatus           : 'ITEM_DISPLAY_STATUS',
+                                                            simpleConditionalDisplay    : 'SIMPLE_CONDITIONAL_DISPLAY']
 
     DataModelService dataModelService
     SimpleExcelDataModelImporterProviderService simpleExcelDataModelImporterProviderService
     OpenClinicaV3CrfProfileProviderService openClinicaV3CrfProfileProviderService
+    OpenClinicaV3CrfSectionProfileProviderService openClinicaV3CrfSectionProfileProviderService
 
     @Override
     String getDisplayName() {
@@ -98,11 +133,7 @@ class OpenClinicaV3CrfDataModelImporterProviderService extends DataModelImporter
             Map<String, String> crfValues = crfSheetValues.findAll {it.crfName && it.version}.last()
 
             dataModel = new DataModel(label: crfValues.crfName, type: DataModelType.DATA_ASSET)
-            String crfProfileNamespace = openClinicaV3CrfProfileProviderService.getMetadataNamespace()
-            dataModel.addToMetadata(new Metadata(namespace: crfProfileNamespace, key: 'crf_name', value: crfValues.crfName))
-            dataModel.addToMetadata(new Metadata(namespace: crfProfileNamespace, key: 'version', value: crfValues.version))
-            dataModel.addToMetadata(new Metadata(namespace: crfProfileNamespace, key: 'version_description', value: crfValues.versionDescription))
-            dataModel.addToMetadata(new Metadata(namespace: crfProfileNamespace, key: 'revision_notes', value: crfValues.revisionNotes))
+            addMetadataFromColumnValues(dataModel, openClinicaV3CrfProfileProviderService, CRF_SHEET_COLUMNS, crfValues)
 
             // Import Sections as tree of Data Classes
             Sheet sectionsSheet = workbook.getSheet('Sections')
@@ -113,13 +144,41 @@ class OpenClinicaV3CrfDataModelImporterProviderService extends DataModelImporter
 
             List<Map<String, String>> sectionsSheetValues = simpleExcelDataModelImporterProviderService.getSheetValues(SECTIONS_SHEET_COLUMNS, sectionsSheet)
 
+            List<DataClass> sections = []
             sectionsSheetValues.each {Map<String, String> sectionValues ->
-                dataModel.addToDataClasses(new DataClass(label: sectionValues.sectionLabel, description: sectionValues.sectionTitle))
+                sections << new DataClass(label: sectionValues.sectionLabel, description: sectionValues.sectionTitle).tap {DataClass dataClass ->
+                    addMetadataFromColumnValues(dataClass, openClinicaV3CrfSectionProfileProviderService, SECTIONS_SHEET_COLUMNS, sectionValues)
+                }
             }
+            sectionsSheetValues.eachWithIndex {Map<String, String> sectionValues, Integer i ->
+                if (sectionValues.parentSection) {
+                    DataClass parentClass = sections.find {it.label == sectionValues.parentSection}
+                    if (parentClass) parentClass.addToDataClasses(sections[i])
+                    else dataModel.addToDataClasses(sections[i])
+                } else {
+                    dataModel.addToDataClasses(sections[i])
+                }
+            }
+
+            // Import Items as Data Elements and Groups as optional nested Data Classes
+
         }
 
         dataModelService.checkImportedDataModelAssociations(currentUser, dataModel)
         dataModel
+    }
+
+    MetadataAware addMetadataFromColumnValues(MetadataAware metadataAware, ProfileProviderService profileProviderService, Map<String, String> columnMap,
+                                              Map<String, String> sheetValues) {
+        String metadataNamespace = profileProviderService.getMetadataNamespace()
+        Profile profile = profileProviderService.getNewProfile()
+        columnMap.each {Map.Entry<String, String> entry ->
+            profile.allFields.find {it.metadataPropertyName.equalsIgnoreCase(entry.value)}?.with {
+                if (sheetValues[entry.key])
+                    metadataAware.addToMetadata namespace: metadataNamespace, key: it.metadataPropertyName, value: sheetValues[entry.key]
+            }
+        }
+        metadataAware
     }
 
     @Override
